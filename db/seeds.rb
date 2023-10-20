388cbba4d18af97a49a1a0196cd16ec45e9458ff
Richard Wise <richard.wise@hey.com>
2023-10-20 16:12:17 -0400

    Create first model and crud actions via admin panel

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+DEFAULT_EQUIPMENT = %W(battle_ropes	bench dumbbell fitness_ball foam_roller jump_rope kettlebell medicine_ball pull_seld punching_bag push_sled resistance_band yoga_mat)
+
+DEFAULT_EQUIPMENT.each do |equipment|
+    Equipment.create! name: equipment.tr("_", " ").titleize
+end
