e4f382e07727a69e7701aa4e6580c1cec22f68ff
Richard Wise <richard.wise@hey.com>
2023-10-20 19:53:41 -0400

    adjustments to standardrb

--- app/controllers/admin/equipment_controller.rb
+++ app/controllers/admin/equipment_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Admin
   class EquipmentController < ApplicationController
     layout 'admin'
--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Equipment < ApplicationRecord
   validates :name, presence: true
 end
